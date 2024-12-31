9fe5791b37d7c07d64974d7ec0fb63ef0f598608
Phùng Hùng <vhung98vp@gmail.com>
2024-12-31 15:05:36 +0700

    Update capital func

--- src/ocr/extract_data.py
+++ src/ocr/extract_data.py
@@ -1,6 +1,6 @@
 import re
 import json
-from .process_ocr import extract_text_from_pdf, write_text_to_txt
+from process_ocr import extract_text_from_pdf, write_text_to_txt
 
 # Patterns for each piece of information
 COMPANY_PATTERNS = {
@@ -12,7 +12,7 @@ COMPANY_PATTERNS = {
     "tel": r"Điện thoại:\s*(.*?)\n",
     #"email": r"Email:\s*(.*?{50}$)\nWebsite",
     "bank_account": r"Số tài khoản:\s*(.*?)\n",
-    "capital": r"Vốn điều lệ:\s*(.*?)\nđồng",
+    "capital": r"Vốn điều lệ:\s*([0-9.\s\n]+)\nđồng",
     "representative": {
         "full_name": r"Người đại diện theo pháp luật\nHọ và tên:\s*(.*?)\n",
         "dob": r"Sinh ngày:\s*(\d{2}/\d{2}/\d{4})\n",
@@ -24,6 +24,7 @@ COMPANY_PATTERNS = {
 
 #DOC_IDS_PATTERN = re.compile(r'[\s\n]([A-Z]\d{7,8}|\d{12}|\d{9})[\s\n]', re.DOTALL)
 DOC_IDS_PATTERN = re.compile(r'([A-Z]\d{7,8}|\d{12}|\d{9})[\s\n]', re.DOTALL)
+MAX_NUM_PATTERN = re.compile(r'\d{1,3}(?:\.\d{3})+')
 
 
 # Exact doc ids
@@ -31,9 +32,18 @@ def find_doc_ids(text, pattern=DOC_IDS_PATTERN):
     matches = list(set(pattern.findall(text)))
     return matches
 
+def find_max_capital(text, current, pattern=MAX_NUM_PATTERN):
+    def to_number(num_string):
+        return int(num_string.replace('.', ''))
+    
+    matches = pattern.findall(text)
+    if current != 'N/A':
+        matches.append(current.replace(' ', ''))
+    return max(matches, key=to_number)
+
 
 # Extract information
-def extract_info(text, data_patterns=COMPANY_PATTERNS):
+def extract_company_info(text, data_patterns=COMPANY_PATTERNS):
     # Define a function to extract information using regex
     def extract_info(pattern, text, group=1, default="N/A"):
         match = re.search(pattern, text, re.DOTALL)
@@ -66,8 +76,9 @@ def extract_data_from_pdfs(pdfs_path, quick_search=True, output_file=None):
             full_text.extend(text)
     
     write_text_to_txt(full_text, f"{pdfs_path[0]}.txt")
-    company_info = extract_info(full_text[0])
+    company_info = extract_company_info(full_text[0])
     company_info["shareholders"] = find_doc_ids('\n'.join(full_text[1:]))
+    company_info["capital"] = find_max_capital('\n'.join(full_text[1:]), company_info["capital"])
 
     if output_file:
         with open(output_file, "w", encoding="utf-8") as f:
