38cd4295bb859893359e8632d0660a3dc49820a2
Phùng Hùng <vhung98vp@gmail.com>
2025-01-02 11:07:31 +0700

    Add google search mst

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from src.captcha.company_tax_id import get_company_tax_id_from_google
 from src.captcha.captcha_solver import get_pdfs_from_page
 # from src.ocr.extract_data import extract_data_from_pdfs
 from src.gemeni_api.gemini import extract_data_from_pdfs
+from src.mst.company_data import get_company_info_from_site
+from src.mst.company_url import get_company_url_from_google
+
 import time
 
 app = Flask(__name__)
@@ -45,6 +48,27 @@ def search_company_data():
     
     except Exception as e:
         return jsonify({"error": f"An error occurred: {e}"})
+
+
+@app.route('/search_google', methods=['GET'])
+def search_company_data():
+    # Site and company for searching
+    site_url = "masothue.com"
+    company_name = request.args.get('company_name')
+    if not company_name:
+        return jsonify({"error": "Company name is required"}), 400
+
+    quick_search = request.args.get('quick')
+    quick_search = True if quick_search == None else quick_search.lower() in ['true', '1', 'yes', 'y']
+    # Get company url site from google, then extract data
+
+    company_url = get_company_url_from_google(company_name, site_url)
+    
+    if quick_search:
+        return {'company_tax_id': re.search(r'\d+', company_url).group()}
+    else:
+        company_info = get_company_info_from_site(company_url)
+        return jsonify(company_info)
 
 if __name__ == "__main__":
     app.run(debug=True)
