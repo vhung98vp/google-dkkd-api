a342c558e5af3a01b16f7e8ce6ed7275c91fd8bd
Phùng Hùng <vhung98vp@gmail.com>
2025-02-11 17:03:12 +0700

    Fix proxy server

--- src/chromedriver/proxies.py
+++ src/chromedriver/proxies.py
@@ -21,17 +21,20 @@ def get_proxy():
                 proxy_list = response.json()
                 if len(proxy_list) > 0:
                     port = random.choice(proxy_list).get("proxy_port")
-                    logger.info(f"Get proxy string config: {proxy_server}:{port}")
-                    return f"{proxy_server}:{port}"
+                    proxy_string = f"{proxy_server.split(':')[0]}:{port}"
+                    logger.info(f"Get proxy string config: {proxy_string}")
+                    return proxy_string
         except Exception as e:
             logger.error("Error, Fail to get proxy: ", e)
 
     return None
 
 def reset_proxy(current_proxy):
-    if current_proxy:
+    load_dotenv()
+    proxy_server = os.getenv('PROXY_SERVER')
+    if current_proxy & proxy_server:
         try:
-            proxy_server, port = current_proxy.split(":")
+            _, port = current_proxy.split(":")
             response = requests.get(f"{proxy_server}/reset?proxy={port}")
             time.sleep(5)
             if response.status_code == 200:
